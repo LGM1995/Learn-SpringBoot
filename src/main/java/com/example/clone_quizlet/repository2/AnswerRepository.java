158d773dfc53633fe2b223969277fabcfef6a9d1
lgm1995 <rjsahr5045@gmail.com>
2022-10-05 21:37:29 +0900

    Feat: All

--- src/main/java/com/example/clone_quizlet/repository2/AnswerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.clone_quizlet.repository2;
-
-import com.example.clone_quizlet.entity2.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-
-}
